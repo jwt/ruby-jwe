e93c939d611434a4b6ec7d3e7a05a90442b946f9
Ilya Scharrenbroich <ilya.j.s@gmail.com>
2019-07-17 13:37:52 -0400

    Enabling code coverage

--- jwe.gemspec
+++ jwe.gemspec
@@ -20,5 +20,7 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'rspec'
   s.add_development_dependency 'rake'
   s.add_development_dependency 'simplecov'
+  s.add_development_dependency 'simplecov-json'
   s.add_development_dependency 'codeclimate-test-reporter'
+  s.add_development_dependency 'codacy-coverage'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,18 @@
 require 'rspec'
 require 'simplecov'
+require 'simplecov-json'
+require 'codeclimate-test-reporter'
+require 'codacy-coverage'
 
 require 'jwe'
+
+Codacy::Reporter.start
+
+SimpleCov.configure do
+  root File.join(File.dirname(__FILE__), '..')
+  project_name 'Ruby JWE - Ruby JSON Web Encryption implementation'
+  add_filter 'spec'
+end
 
 SimpleCov.start if ENV['COVERAGE']
 
